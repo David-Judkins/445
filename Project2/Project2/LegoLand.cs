f06c65e34d8d693e50ea18ed121f61c12efeb3b0
Anna Mcdonald <anya5682@gmail.com>
2020-09-26 23:02:19 -0700

    LegoLand

--- Project2/Project2/LegoLand.cs
+++ Project2/Project2/LegoLand.cs
@@ -6,11 +6,12 @@ using System.Threading;
 
 namespace Project2
 {
-    public delegate void priceCutEventLL(double pr, int pDiff, string parkName);
+    public delegate void priceCutEventLL(double pr, int pDiff, string parkName); // Define a delegate 
     class LegoLand
     {
         static Random rng = new Random(); // To generate random numbers 
         public static event priceCutEventLL priceCut; // Link event to delegate 
+        private OrderProcessor OP = new OrderProcessor();
         private static int ticketPrice = 10;
         private static int priceCutCount = 0;
         private static int priceDiff;
@@ -20,11 +21,14 @@ namespace Project2
         {
             if (price < ticketPrice)
             {    // a price cut 
-                if (priceCut != null)  // there is at least a subscriber
-                 priceDiff = ticketPrice - price;
-                priceCut(price, priceDiff, "LegoLand");    // emit event to subscribers
-                priceDiff = ticketPrice - price;
-                ticketPrice = price;
+                if (priceCut != null)
+                {// there is at least a subscriber
+                    priceDiff = ticketPrice - price;
+                    priceCut(price, priceDiff, "LegoLand");    // emit event to subscribers
+                   
+                    ticketPrice = price;
+                    priceCutCount++;
+                }
             }
             ticketPrice = price;
         }
@@ -33,6 +37,16 @@ namespace Project2
             while(priceCutCount < 20)
             {
                 Thread.Sleep(500);
+                OrderClass order = null;
+                eCommerce.rwLock.AcquireReaderLock(300);
+                try
+                {
+                    order = eCommerce.buffer.getACell();
+                }
+                finally
+                {
+                    eCommerce.rwLock.ReleaseReaderLock();
+                }
                 // Take the order from the queue of the orders; // Decide the price based on the orders
                 int p = rng.Next(80, 300); // Console.WriteLine("New Price is {0}", p);
                 LegoLand.changePrice(p);
